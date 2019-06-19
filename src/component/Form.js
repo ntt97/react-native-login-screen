36586db8b06ad0d40514848dc6da3a7c21e3ddcd
ntt97 <nthanhto1006@gmail.com>
2019-06-20 06:52:06 +0700

    solution

---
+++ src/component/Form.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import {View,Text} from 'react-native'
+
+export default class From extends Component{
+    render(){
+        return(
+            <View style={{flex:1}}></View>
+        )
+    }
+}
--- src/component/LoginScreen.js
+++ src/component/LoginScreen.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import { ImageBackground} from 'react-native';
 import Logo from './Logo'
+
+import From from './Form';
+
 export default class LoginScreen extends Component{
     render(){
         return(
             <ImageBackground  style={{flex:1}} source={require('../images/wallpaper.png')}>
                 <Logo />
+                <From />
+                
             </ImageBackground>
 
         )
